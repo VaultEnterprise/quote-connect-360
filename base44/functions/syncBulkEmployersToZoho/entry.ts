648d5c1d482ea4363494a661d50f6930071a7008
marconzio1 <36343206+marconzio1@users.noreply.github.com>
2026-04-03 15:34:06 -0700

    chore: bump @base44/sdk to 0.8.23

--- base44/functions/syncBulkEmployersToZoho/entry.ts
+++ base44/functions/syncBulkEmployersToZoho/entry.ts
@@ -5,7 +5,7 @@
  * Usage: base44.functions.invoke('syncBulkEmployersToZoho', { agency_id: 'xyz' })
  */
 
-import { createClientFromRequest } from 'npm:@base44/sdk@0.8.21';
+import { createClientFromRequest } from 'npm:@base44/sdk@0.8.23';
 
 Deno.serve(async (req) => {
   try {
